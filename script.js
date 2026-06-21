fc966d6d8fcb1b36dad7ff38f824c7439978d8d7
Flavia Menck <flavia-menck@hotmail.com>
2026-06-21 13:42:01 -0300

    :tada: initial commit

---
+++ script.js
@@ -0,0 +1,22 @@
+function toggleMode() {
+  const html = document.documentElement
+  html.classList.toggle("light")
+
+  //troca a imagem do perfil
+  const img = document.querySelector("#profile img")
+  //se tiver a classe light, adiciona a imagem light
+  if (html.classList.contains("light")) {
+    img.setAttribute("src", "./assets/avatar-light.png")
+    img.setAttribute(
+      "alt",
+      "Foto de Flávia Menck com um sorriso amigável, cabelos ruivos, olhos castanhos e pele clara. Usando óculos e camiseta cinza clara. Fundo da imagem claro.",
+    )
+    //se não tiver a classe light, adiciona a imagem normal
+  } else {
+    img.setAttribute("src", "./assets/avatar.png")
+    img.setAttribute(
+      "alt",
+      "Foto de Flávia Menck com um sorriso amigável, cabelos ruivos, olhos castanhos e pele clara. Usando óculos e camiseta cinza clara. Fundo da imagem escuro.",
+    )
+  }
+}
